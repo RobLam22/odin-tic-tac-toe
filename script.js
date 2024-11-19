046efdaa3328ccde8c90598bc79c0c82bebc8aab
Robert Lam <robert.lam22@gmail.com>
2024-11-19 14:30:40 +1100

    working console game with user input

--- script.js
+++ script.js
@@ -19,21 +19,54 @@ function createPlayers() {
     return { playerOne, playerTwo }
 }
 
-// Logic for a player turn
+// Logic for the game
 function gameLogic(board, players) {
     const { playerOne, playerTwo } = players
+    let playerTurn = true; // true = Player1, false = Player2
+    let index = 0;
+    
+    // board = [1,1,1,'','','','','','']
+    // console.log(board[8])
+    // winCheck(board[0])
+
+    // LOGIC for PLAYER INPUT
+
     for (i = 0; i < 9; i++) {
-        let input = Number(prompt("Please choose a square [0-9]"))
+        const playerValue = playerTurn ? playerOne.value : playerTwo.value;
+        let inputMsg = playerTurn ? "Player 1: Please choose a square" : "Player 2: Please choose a square"
+        let input = Number(prompt(inputMsg)) // change to div and clicks
         if (board[input] === '') {
-            board[input] = 'X'
+            if (playerTurn) {
+                board[input] = playerValue
+                index = input
+                playerTurn = false
+            } else if (!playerTurn) {
+                board[input] = playerValue
+                index = input
+                playerTurn = true
+            }
             console.log(board)
-            // check win after 5 squares full
+            if (i > 3) {
+                if (winCheck(index, playerValue, board)) {
+                    console.log(`${playerValue} is the winner`)
+                    break
+                }
+            }
         }
     }
 }
 
-function winCheck() {
-    
+// 0,1,2
+// 3,4,5
+// 6,7,8
+
+// Win check - still needs to finalise win condition
+function winCheck(index, playerValue, board) {
+    console.log(index, playerValue, board)
+    const winConditions = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
+    let winCombos = winConditions.filter((combo) => combo.includes(index)).some(possibleCombo => possibleCombo.every(index => board[index] === playerValue))
+    return true
+
 }
 
 // Run Game
